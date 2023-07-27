2813f275efd5417b10455034c030df44b772f911
PedagogicoMRJ <pedagogicomyrobotjoinville@gmail.com>
2023-07-27 15:46:16 -0300

    Adicionando componentes

--- MyProfile/App.js
+++ MyProfile/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <ImageBackground style={styles.gradient} source={require("./assets/gradient.png")}>
     <View style={styles.boxProfile}>
       <Image source={require("./assets/profile.png")} style={styles.profileImg}/>
-      <Text> style={styles.nameTxt}Millena Rosa</Text>
-      <Text> style={styles.proTxt}Professora</Text>
+      <Text style={styles.nameTxt}>Millena Rosa</Text>
+      <Text style={styles.proTxt}>Professora</Text>
+      <Text style={styles.text}> Like:</Text>
+      <View style={styles.boxInterest}>
+        <Image source={require('./assets/devices.png')} style={styles.imgInterest}/>
+        <Image source={require('./assets/swiming.png')} style={styles.imgInterest}/>
+        <Image source={require('./assets/camera.png')} style={styles.imgInterest}/>
+      </View>
       </View>
       <StatusBar style="auto" />
     </ImageBackground>
@@ -18,26 +24,48 @@ const styles = StyleSheet.create({
   gradient: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   boxProfile:{
     backgroundColor: 'rgba(250,250,250,0.5)',
     borderRadius: 20,
     padding: 20,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   profileImg:{
     width:120,
     height:120,
-    backgroundColor:'rgba(255,255,255,.2)',
+    backgroundColor:'rgba(255,255,255, 0.2)',
     borderRadius: 60,
     borderColor: 'rgb(0,0,0)',
     borderWidth: 0.3,
-    margin:20
+    margin:20,
   },
-  nameTxt:{
+  naTxt:{
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  proTxt:{
     fontSize:20,
     fontWeight:'300',
-    fontStyle:'italic'
+    fontStyle:'italic',
+  },
+  text:{
+    fontSize:20,
+    fontWeight:'300',
+    marginTop: 25,
+    marginbottom:10,
+  },
+  boxInterest:{
+    flexDirection: 'row',
+    justifyContent:'space-evenly',
+    width:280,
+    backgroundColor: 'rgba(255,255,255,.1)',
+    borderRadius: 15,
+  },
+  imgInterest:{
+    width: 50,
+    height:50,
+    marginVertical: 10,
   },
 });
